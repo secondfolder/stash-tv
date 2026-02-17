093430e0e999cbb0be638feea95f00d4928815b3
Second Folder <secondfolder@gmail.com>
2026-02-17 23:54:47 +1100

    ci: correctly print the new version number

--- release.config.js
+++ release.config.js
@@ -28,7 +28,7 @@ export default {
     [
       "@semantic-release/exec",
       {
-        prepareCmd: "yq -i '.version = \"${nextRelease.version}\"' packages/tv-plugin/dist/stash-tv.yml",
+        prepareCmd: "jq '.version = \"${nextRelease.version}\"' package.json > package.json.tmp && mv package.json.tmp package.json && yq -i '.version = \"${nextRelease.version}\"' packages/tv-plugin/dist/stash-tv.yml",
         successCmd: "./scripts/deploy-to-stash-plugins.sh ${nextRelease.version}",
       },
     ],
